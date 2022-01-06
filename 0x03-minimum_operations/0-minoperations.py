ad7d7a0720380eef4d7f9ce7ebcd31cb8237c307
SebastianMH14 <sebasmh2002@hotmail.com>
2022-01-06 17:29:46 -0500

    [add] task 0

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 """Minimum Operations"""
+import sys
+
 
 def minOperations(n):
-    if n <= 0:
-        return 0
-    for i in range(n):
-        i * n
-    return n
+    res = 0
+    i = 2
+
+    while i <= n:
+        while n % i == 0:
+            res += i
+            n = n / i
+    i += 1
+    return res
