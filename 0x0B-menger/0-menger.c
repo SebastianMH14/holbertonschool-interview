72bf404ddbc4f12d324638ad1d0609eeab59a585
SebastianMH14 <sebasmh2002@hotmail.com>
2022-03-03 21:46:54 -0500

    fix betty

--- 0x0B-menger/0-menger.c
+++ 0x0B-menger/0-menger.c
@@ -8,30 +8,30 @@
  */
 void menger(int level)
 {
-    int len, row, col, drow, dcol, f;
+	int len, row, col, drow, dcol, f;
 
-    len = (int)pow(3, level);
-    for (col = 0; col < len; col++)
-    {
-        for (row = 0; row < len; row++)
-        {
-            drow = row;
-            dcol = col;
-            f = 0;
-            while (drow && dcol)
-            {
-                if (drow % 3 == 1 && dcol % 3 == 1)
-                {
-                    printf(" ");
-                    f = 1;
-                    break;
-                }
-                drow = drow / 3;
-                dcol = dcol / 3;
-            }
-            if (!f)
-                printf("#");
-        }
-        printf("\n");
-    }
+	len = (int)pow(3, level);
+	for (col = 0; col < len; col++)
+	{
+		for (row = 0; row < len; row++)
+		{
+			drow = row;
+			dcol = col;
+			f = 0;
+			while (drow && dcol)
+			{
+				if (drow % 3 == 1 && dcol % 3 == 1)
+				{
+					printf(" ");
+					f = 1;
+					break;
+				}
+				drow = drow / 3;
+				dcol = dcol / 3;
+			}
+			if (!f)
+				printf("#");
+		}
+		printf("\n");
+	}
 }
