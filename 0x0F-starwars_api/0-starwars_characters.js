aa7753c5fa956d783f43ad6d98979a5802233678
SebastianMH14 <sebasmh2002@hotmail.com>
2022-04-07 20:13:46 -0500

    Fix task 0

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -3,15 +3,15 @@ const request = require('request');
 
 const args = process.argv.slice(2);
 
-request(`https://swapi-api.hbtn.io/api/films/${args[0]}/`, (error, response, body) => {
+request(`https://swapi-api.hbtn.io/api/films/${args[0]}/`, async (error, response, body) => {
   if (error) {
     console.error(error);
   } else {
     const json = JSON.parse(body);
-    json.characters.forEach((character) => {
+    await json.characters.forEach((character) => {
       request(character, (error, response, body) => {
         if (error) {
-          console.error(error);
+          console.log(error);
         } else {
           const json = JSON.parse(body);
           console.log(json.name);
