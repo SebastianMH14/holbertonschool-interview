174cfd4a39f65affb490614686380bf1e4ac6457
SebastianMH14 <sebasmh2002@hotmail.com>
2022-04-07 20:46:43 -0500

    Fix semistandar

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -3,13 +3,17 @@ const request = require('request');
 
 const args = process.argv.slice(2);
 
-request('https://swapi-api.hbtn.io/api/films/' + args[0], async (err, response, body) => {
+request(`https://swapi-api.hbtn.io/api/films/${args[0]}`, async (err, response, body) => {
   if (err) console.log(err);
   for (const name of JSON.parse(body).characters) {
     await new Promise((resolve, reject) => {
       request(name, (err, response, body) => {
-        console.log(JSON.parse(body).name);
-        resolve();
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(JSON.parse(body).name);
+          resolve();
+        }
       });
     });
   }
