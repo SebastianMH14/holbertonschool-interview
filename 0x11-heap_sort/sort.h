26fbcbe1dfe26aab2ce410ec655222988a752f66
SebastianMH14 <sebasmh2002@hotmail.com>
2022-04-21 21:59:02 -0500

    Fix task 0

--- 0x11-heap_sort/sort.h
+++ 0x11-heap_sort/sort.h
@@ -1,8 +1,8 @@
-#ifndef _HOLBERTON_
-#define _HOLBERTON_
+#ifndef SORT_H
+#define SORT_H
 
 #include <stdlib.h>
-
+#include <stdio.h>
 
 void print_array(const int *array, size_t size);
 void heap_sort(int *array, size_t size);
