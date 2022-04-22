c8f018d059ec51eb933906008ac3b2c0559b3215
SebastianMH14 <sebasmh2002@hotmail.com>
2022-04-21 20:39:32 -0500

    Add task 0

---
+++ 0x11-heap_sort/0-heap_sort.c
@@ -0,0 +1,60 @@
+#include "sort.h"
+
+/**
+ * swap - Swaps two elements of an array
+ * @a: First element
+ * @b: Second element
+ * return: void
+ */
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
+ * heapify - Heapify the array
+ * @arr: The array to be heapified
+ * @n: The size of the array
+ * @i: The index of the current node
+ * Return: void
+ */
+void heapify(int arr[], int n, int i)
+{
+	int largest = i;
+	int left = 2 * i + 1;
+	int right = 2 * i + 2;
+
+	if (left < n && arr[left] > arr[largest])
+		largest = left;
+
+	if (right < n && arr[right] > arr[largest])
+		largest = right;
+
+	if (largest != i)
+	{
+		swap(&arr[i], &arr[largest]);
+		heapify(arr, n, largest);
+	}
+}
+
+/**
+ * heap_sort - Sorts an array of integers in ascending order using the heap
+ * sort algorithm
+ *
+ * @array: The array to be sorted
+ * @size: Number of elements in @array
+ */
+void heap_sort(int *array, size_t size)
+{
+	for (int i = size / 2 - 1; i >= 0; i--)
+		heapify(array, size, i);
+
+	for (int i = size - 1; i >= 0; i--)
+	{
+		swap(&array[0], &array[i]);
+
+		heapify(array, i, 0);
+	}
+}
---
+++ 0x11-heap_sort/sort.h
@@ -0,0 +1,13 @@
+#ifndef _HOLBERTON_
+#define _HOLBERTON_
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
+void print_array(const int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void swap(int *a, int *b);
+void heapify(int arr[], int n, int i);
+
+#endif
