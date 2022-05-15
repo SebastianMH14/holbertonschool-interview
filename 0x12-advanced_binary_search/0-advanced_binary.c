2ccbdf537169c133bc1fdd44619a4d90955f46c9
SebastianMH14 <sebasmh2002@hotmail.com>
2022-05-15 15:25:14 -0500

    Add task 0

---
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -0,0 +1,52 @@
+#include "binary_search.h"
+
+/**
+ * advanced_binary - binary search
+ * @array: array to search
+ * @size: size of array
+ * @value: value to search for
+ *
+ * Return: index of value, -1 if not found
+ */
+int advanced_binary(int *array, size_t size, int value)
+{
+	if (array == NULL || size < 1)
+		return (-1);
+
+	return (binary_search(array, 0, size - 1, value));
+}
+
+/**
+ * binary_search- advancend binary search
+ * @array:  array to search
+ * @left:  left array
+ * @right:  right array
+ * @value: value to find
+ *  Return:  int
+ */
+int binary_search(int *array, int left, int right, int value)
+{
+	int mid = (left + right) / 2;
+	int i;
+
+	if (left > right)
+		return (-1);
+
+	printf("Searching in array: ");
+
+	for (i = left; i <= right; i++)
+	{
+		if (i < right)
+			printf("%d, ", array[i]);
+		else
+			printf("%d\n", array[i]);
+	}
+
+	if (array[left] == value)
+		return (left);
+
+	if (array[mid] < value)
+		return (binary_search(array, mid + 1, right, value));
+
+	return (binary_search(array, left, mid, value));
+}
