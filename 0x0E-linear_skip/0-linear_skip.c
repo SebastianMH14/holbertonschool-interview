e023a7f3b3f93a0ddb1c36194ca16048a2038f53
SebastianMH14 <sebasmh2002@hotmail.com>
2022-08-17 10:16:01 -0500

    Fix linear search

--- 0x0E-linear_skip/0-linear_skip.c
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -1,47 +1,43 @@
 #include "search.h"
-
-
 /**
- * main - Entry point
+ * linear_skip - Searches for a values.
+ * @list: pointer
+ * @value: value t
  *
- * Return: Always EXIT_SUCCESS
+ * Return: pointer
  */
-skiplist_t *linear_skip(skiplist_t *list, int value) 
+skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-    skiplist_t *current = list, *find = list;
+	skiplist_t *prev = list;
 
-    if (list == NULL)
-        return (NULL);
+	if (!list)
+		return (NULL);
 
-    while (find || find->express != NULL || find->n < value)
-    {
-        current = find;
-        find = find->express;
-        printf("Value checked at index [%lu] = [%d]\n", find->index, find->n);
-    }
+	while (list->n < value && list->express)
+	{
+		prev = list;
+		list = list->express;
+		printf("Value checked at index [%ld] = [%d]\n", list->index, list->n);
+	}
 
-    if (!(find->express) || !(find->n > value))
-    {
-        current = find;
-        while (find -> next)
-        {
-            find = find->next;
-        }
-    }
+	if (value <= list->n)
+		printf("Value found between indexes [%ld] and [%ld]\n",
+				prev->index, list->index);
+	else
+	{
+		prev = prev->express;
+		while (list->next)
+			list = list->next;
+		printf("Value found between indexes [%ld] and [%ld]\n",
+				prev->index, list->index);
+	}
 
-    printf("Value found between indexes [%lu] and [%lu]\n",
-		   current->index, find->index);
-
-    while (current || current->n < value)
-    {
-        printf("Value checked at index [%lu] = [%d]\n", current->index, current->n);
-        current = current->next;
-    }
-
-    if (current)
-    {
-        printf("Value checked at index [%lu] = [%d]\n", current->index, current->n);
-    }
-
-    return (current && current->n == value ? current : NULL);
+	while (prev)
+	{
+		printf("Value checked at index [%ld] = [%d]\n", prev->index, prev->n);
+		if (prev->n == value)
+			return (prev);
+		prev = prev->next;
+	}
+	return (NULL);
 }
